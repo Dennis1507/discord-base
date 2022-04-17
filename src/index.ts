278e038fcc652cbe7af071d1c0bb5e07e7eb6454
Dennis <34622422+Dennis1507@users.noreply.github.com>
2022-04-17 15:20:27 +0200

    Context Menu Support (#5)

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import config from '../config.json';
 
 export class EnhancedClient extends Client {
 	public commands: Collection<string, (...args: any[]) => void> = new Collection();
+	public usercommands: Collection<string, (...args: any[]) => void> = new Collection();
+	public messagecommands: Collection<string, (...args: any[]) => void> = new Collection();
 	public config: Record<string, string> = config;
 
 	public constructor(options: ClientOptions) {
@@ -16,9 +18,11 @@ export class EnhancedClient extends Client {
 const client = new EnhancedClient({ intents: Intents.FLAGS.GUILDS });
 
 client.on('interactionCreate', async interaction => {
-	if (interaction.isCommand()) {
-		const command = client.commands.get(interaction.commandName);
-		if (command) command(interaction);
-		else interaction.reply({ content: 'Unknown command.', ephemeral: true });
-	}
+	let command;
+	if (interaction.isCommand()) { command = client.commands.get(interaction.commandName); }
+	else if (interaction.isUserContextMenu()) { command = client.usercommands.get(interaction.commandName); }
+	else if (interaction.isMessageContextMenu()) { command = client.messagecommands.get(interaction.commandName); }
+	else { return; }
+	if (command) command(interaction);
+	else interaction.reply({ content: 'Unknown command.', ephemeral: true });
 });
